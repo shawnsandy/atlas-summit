9d5b78124748d43a63f8695a753b075e8af6e84f
Unknown <steve@ninjagamehost.com>
2017-05-12 13:37:38 -0700

    Added Sessions

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sessions;
+use Illuminate\Http\Request;
+
+class SessionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Sessions  $sessions
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sessions $sessions)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Sessions  $sessions
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sessions $sessions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Sessions  $sessions
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sessions $sessions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Sessions  $sessions
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sessions $sessions)
+    {
+        //
+    }
+}
