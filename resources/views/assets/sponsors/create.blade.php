2e662e0942ab6ff2a8b4fd4a797693521ca3785a
shawnsandy04 <shawnsandy04@gmail.com>
2017-05-13 19:21:15 -0400

    reset col size 9/3 to 8/4

--- resources/views/assets/sponsors/create.blade.php
+++ resources/views/assets/sponsors/create.blade.php
@@ -2,12 +2,12 @@
 @section("content")
 
     <div class="container-fluid">
-        <div class="col-md-9 form-component">
+        <div class="col-md-8 form-component">
             @component("dash::components.panels.dashboard", ["title" => "Add Sponsors"])
                 {{ Form::createForm('App\Sponsor', "sponsors") }}
             @endcomponent
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             @component("dash::components.panels.widget", ["title" => "Regions Registered"])
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, vitae!
             @endcomponent
--- resources/views/assets/sponsors/edit.blade.php
+++ resources/views/assets/sponsors/edit.blade.php
@@ -2,12 +2,12 @@
 @section("content")
 
     <div class="container-fluid">
-        <div class="col-md-9">
+        <div class="col-md-8">
             @component("dash::components.panels.dashboard")
                 {{ Form::editForm('App\Sponsor', "sponsors") }}
             @endcomponent
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             @component("dash::components.panels.widget", ["title" => "Sponsors List"]);
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, vitae!</p>
             @endcomponent
