454f5e57630bf344c5794a9a1bdcc622d61da1c1
Unknown <steve@ninjagamehost.com>
2017-05-17 15:15:19 -0700

    Added Sponsors index

--- app/Http/Controllers/SponsorsController.php
+++ app/Http/Controllers/SponsorsController.php
@@ -16,7 +16,9 @@ class SponsorsController extends Controller
      */
     public function index()
     {
-        //
+        $sponsors = Sponsor::all();
+
+        return view("sponsors.index", compact('sponsors'));
     }
 
     /**
