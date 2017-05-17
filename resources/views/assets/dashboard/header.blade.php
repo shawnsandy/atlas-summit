9e0eaed67ad8560856f5af0377a4177035f2c569
Unknown <steve@ninjagamehost.com>
2017-05-17 15:18:44 -0700

    Fixed urls for all assets.

--- resources/views/assets/dashboard/header.blade.php
+++ resources/views/assets/dashboard/header.blade.php
@@ -5,16 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Summit Dashboard</title>
 
-    <link href="css/admin/bootstrap.min.css" rel="stylesheet">
-    <link href="css/admin/datepicker3.css" rel="stylesheet">
-    <link href="css/admin/styles.css" rel="stylesheet">
+    <link href="/css/admin/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/admin/datepicker3.css" rel="stylesheet">
+    <link href="/css/admin/styles.css" rel="stylesheet">
 
     <!--Icons-->
-    <script src="js/admin/lumino.glyphs.js"></script>
+    <script src="/js/admin/lumino.glyphs.js"></script>
 
     <!--[if lt IE 9]>
-    <script src="js/admin/html5shiv.js"></script>
-    <script src="js/admin/respond.min.js"></script>
+    <script src="/js/admin/html5shiv.js"></script>
+    <script src="/js/admin/respond.min.js"></script>
     <![endif]-->
 
 </head>
