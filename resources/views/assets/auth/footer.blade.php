8fd3c3fda3b45799c2c3c96447754aa87678e1c1
Unknown <steve@ninjagamehost.com>
2017-05-18 13:18:57 -0700

    Added bootstrap to Auth Layout

--- resources/views/assets/auth/footer.blade.php
+++ resources/views/assets/auth/footer.blade.php
@@ -4,6 +4,7 @@
         $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
     });
 </script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </body>
 </html>
--- resources/views/assets/auth/header.blade.php
+++ resources/views/assets/auth/header.blade.php
@@ -12,5 +12,6 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/auth.css') }}" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
