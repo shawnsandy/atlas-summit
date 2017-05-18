132224bda6887499afc9522a5ae8fc44523b6f59
Unknown <steve@ninjagamehost.com>
2017-05-18 09:04:36 -0700

    Fixed my fucking ugly ass if statemens with a clean solution.

--- resources/views/assets/dashboard/side-bar.blade.php
+++ resources/views/assets/dashboard/side-bar.blade.php
@@ -5,53 +5,28 @@
         </div>
     </form>
     <ul class="nav menu">
-        @if((Request::path() == 'admin'))
-            <li class="active">
-        @else
-            <li>
-        @endif
-                <a href="/admin"><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg> Dashboard</a>
-            </li>
+        <li class="{{ Request::is('admin') ? 'active' : ''}}">
+            <a href="/admin"><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg> Dashboard</a>
+        </li>
 
-        @if((Request::path() == 'admin/regions'))
-            <li class="active">
-        @else
-            <li>
-        @endif
-                <a href="/admin/regions"><svg class="glyph stroked map"><use xlink:href="#stroked-map"/></svg> Regions</a>
-            </li>
-        @if((Request::path() == 'admin/rooms'))
-            <li class="active">
-        @else
-            <li>
-        @endif
-            <a href="/admin/rooms"><svg class="glyph stroked home"><use xlink:href="#stroked-home"/></svg>
-                Rooms</a>
-            </li>
+        <li class="{{ Request::is('admin/regions', 'admin/regions/*') ? 'active' : ''}}">
+            <a href="/admin/regions"><svg class="glyph stroked map"><use xlink:href="#stroked-map"/></svg> Regions</a>
+        </li>
+        <li class="{{ Request::is('admin/rooms', 'admin/rooms/*') ? 'active' : ''}}">
+            <a href="/admin/rooms"><svg class="glyph stroked home"><use xlink:href="#stroked-home"/></svg> Rooms</a>
+        </li>
 
-        @if((Request::path() == 'admin/sponsors'))
-            <li class="active">
-        @else
-            <li>
-        @endif
-                <a href="/admin/sponsors"><svg class="glyph stroked star"><use xlink:href="#stroked-star"/></svg> Sponsors</a>
-            </li>
+        <li class="{{ Request::is('admin/sponsors', 'admin/sponsors/*') ? 'active' : ''}}">
+            <a href="/admin/sponsors"><svg class="glyph stroked star"><use xlink:href="#stroked-star"/></svg> Sponsors</a>
+        </li>
 
-        @if((Request::path() == 'admin/users'))
-            <li class="active">
-        @else
-            <li>
-        @endif
-                <a href="/admin/users"><svg class="glyph stroked male user "><use xlink:href="#stroked-male-user"/></svg> Users</a>
-            </li>
+        <li class="{{ Request::is('admin/users', 'admin/users/*') ? 'active' : ''}}">
+            <a href="/admin/users"><svg class="glyph stroked male user "><use xlink:href="#stroked-male-user"/></svg> Users</a>
+        </li>
 
-        @if((Request::path() == 'admin/workshops'))
-          <li class="active">
-        @else
-          <li>
-        @endif
+        <li class="{{ Request::is('admin/workshops', 'admin/workshops/*') ? 'active' : ''}}">
              <a href="/admin/workshops"><svg class="glyph stroked pencil"><use xlink:href="#stroked-pencil"/></svg> Workshops</a>
-          </li>
+        </li>
     </ul>
     <div class="attribution">
         Powered by <a href="http://atlasforworkforce.com/">ATLAS</a><br/>
