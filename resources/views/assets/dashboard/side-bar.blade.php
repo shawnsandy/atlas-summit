c09388cda950eb45a69cf88dacd4f029e2a0eb3f
Unknown <steve@ninjagamehost.com>
2017-05-18 07:18:11 -0700

    Active class now works properly on dashboard sidebar

--- resources/views/assets/dashboard/side-bar.blade.php
+++ resources/views/assets/dashboard/side-bar.blade.php
@@ -5,35 +5,55 @@
         </div>
     </form>
     <ul class="nav menu">
-        <li class="active"><a href="/"><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg> Dashboard</a></li>
-        <li><a href="/admin/regions"><svg class="glyph stroked map"><use xlink:href="#stroked-map"/></svg> Regions</a></li>
-        <li><a href="/admin/rooms"><svg class="glyph stroked home"><use xlink:href="#stroked-home"/></svg>
-                Rooms</a></li>
-        <li><a href="/admin/sponsors"><svg class="glyph stroked star"><use xlink:href="#stroked-star"/></svg> Sponsors</a></li>
-        <li><a href="/admin/users"><svg class="glyph stroked male user "><use xlink:href="#stroked-male-user"/></svg> Users</a></li>
-        <li><a href="/admin/workshops"><svg class="glyph stroked pencil"><use xlink:href="#stroked-pencil"/></svg> Workshops</a></li>
-        {{--<li class="parent ">--}}
-            {{--<a href="#">--}}
-                {{--<span data-toggle="collapse" href="#sub-item-1"><svg class="glyph stroked chevron-down"><use xlink:href="#stroked-chevron-down"></use></svg></span> Dropdown--}}
-            {{--</a>--}}
-            {{--<ul class="children collapse" id="sub-item-1">--}}
-                {{--<li>--}}
-                    {{--<a class="" href="#">--}}
-                        {{--<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> Sub Item 1--}}
-                    {{--</a>--}}
-                {{--</li>--}}
-                {{--<li>--}}
-                    {{--<a class="" href="#">--}}
-                        {{--<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> Sub Item 2--}}
-                    {{--</a>--}}
-                {{--</li>--}}
-                {{--<li>--}}
-                    {{--<a class="" href="#">--}}
-                        {{--<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> Sub Item 3--}}
-                    {{--</a>--}}
-                {{--</li>--}}
-            {{--</ul>--}}
-        {{--</li>--}}
+        @if((Request::path() == 'admin'))
+            <li class="active">
+        @else
+            <li>
+        @endif
+                <a href="/admin"><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg> Dashboard</a>
+            </li>
+
+        @if((Request::path() == 'admin/regions'))
+            <li class="active">
+        @else
+            <li>
+        @endif
+                <a href="/admin/regions"><svg class="glyph stroked map"><use xlink:href="#stroked-map"/></svg> Regions</a>
+            </li>
+        @if((Request::path() == 'admin/rooms'))
+            <li class="active">
+        @else
+            <li>
+        @endif
+            <a href="/admin/rooms"><svg class="glyph stroked home"><use xlink:href="#stroked-home"/></svg>
+                Rooms</a>
+            </li>
+
+        @if((Request::path() == 'admin/sponsors'))
+            <li class="active">
+        @else
+            <li>
+        @endif
+                <a href="/admin/sponsors"><svg class="glyph stroked star"><use xlink:href="#stroked-star"/></svg> Sponsors</a>
+            </li>
+
+        @if((Request::path() == 'admin/users'))
+            <li class="active">
+        @else
+            <li>
+        @endif
+                <a href="/admin/users"><svg class="glyph stroked male user "><use xlink:href="#stroked-male-user"/></svg> Users</a>
+            </li>
+
+        @if((Request::path() == 'admin/workshops'))
+          <li class="active">
+        @else
+          <li>
+        @endif
+             <a href="/admin/workshops"><svg class="glyph stroked pencil"><use xlink:href="#stroked-pencil"/></svg> Workshops</a>
+          </li>
     </ul>
-    <div class="attribution">Powered by <a href="http://www.medialoot.com/item/lumino-admin-bootstrap-template/">ATLAS</a><br/><a href="http://www.glyphs.co" style="color: #333;">Icons by Glyphs</a></div>
+    <div class="attribution">
+        Powered by <a href="http://atlasforworkforce.com/">ATLAS</a><br/>
+    </div>
 </div><!--/.sidebar-->
