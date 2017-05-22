9730c5da3783c43f1a0b35acd00e416fff97a6bd
shawnsandy04 <shawnsandy04@gmail.com>
2017-05-22 19:49:36 -0400

    increase padding on btn-xs

--- resources/views/components/widgets.blade.php
+++ resources/views/components/widgets.blade.php
@@ -1,6 +1,6 @@
 <div class="panel info-component">
     <div class="clearfix">
-        <div class="col-md-3 h1" style="font-size: 48px">
+        <div class="col-md-3 h1" style="font-size: 36px">
             {{ Html::entypoFont((isset($icon)) ?  $icon : "vinyl") }}
         </div>
         <div class="col-md-9">
