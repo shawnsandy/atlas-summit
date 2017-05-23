e40d678b3ac9b0da0fe15cce0293e0c3b1a44d75
shawn sandy <shawnsandy04@gmail.com>
2017-05-23 14:47:48 -0400

    add user - workshops realitionships

--- app/Workshop.php
+++ app/Workshop.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Workshop extends Model
 {
     protected $fillable = ["name", "description", "date", "start_time", "end_time", "room_id"];
+
+    public function participants(){
+        return $this->belongsToMany(User::class);
+    }
+
+
 }
