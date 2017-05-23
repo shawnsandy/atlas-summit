e5861dcf359ec2d235b30af482bb327f4aa0de90
shawnsandy04 <shawnsandy04@gmail.com>
2017-05-22 21:37:34 -0400

    change the room icon

--- resources/views/vendor/dash/partials/sidebar-menu.blade.php
+++ resources/views/vendor/dash/partials/sidebar-menu.blade.php
@@ -2,7 +2,7 @@
     <a href="/dashboard">{{ Html::entypoFont("network") }} <span class="nav-text">Dashboard</span></a>
 </li>
 <li>
-    <a href="/dashboard/articles">{{ Html::entypoFont("news") }}  <span class="nav-text"> Articles</span></a>
+    <a href="/dashboard/articles">{{ Html::entypoFont("text-document-inverted") }}  <span class="nav-text"> Articles</span></a>
 </li>
 <li>
     <a href="/dashboard/workshops">{{ Html::entypoFont("blackboard") }}  <span class="nav-text"> Workshops</span></a>
@@ -14,11 +14,11 @@
     <a href="{{ url("/dashboard/users") }}">{{ Html::entypoFont("users") }}  <span class="nav-text"> Users</span></a>
 </li>
 <li>
-    <a href="/dashboard/rooms">{{ Html::entypoFont("box") }}  <span class="nav-text"> Rooms</span></a>
+    <a href="/dashboard/rooms">{{ Html::entypoFont("shop") }}  <span class="nav-text"> Rooms</span></a>
 </li>
 <li>
     <a href="/dashboard/sponsors">{{ Html::entypoFont("star") }}  <span class="nav-text"> Sponsors</span></a>
 </li>
-<li>
-    <a href="/dashboard/system">{{ Html::entypoFont("cog") }}  <span class="nav-text"> System</span></a>
-</li>
+{{--<li>--}}
+    {{--<a href="/dashboard/system">{{ Html::entypoFont("cog") }}  <span class="nav-text"> System</span></a>--}}
+{{--</li>--}}
