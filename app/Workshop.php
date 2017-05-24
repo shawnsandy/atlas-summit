fd265c0e919029daaeff2de80406c41a242d051d
shawn sandy <shawnsandy04@gmail.com>
2017-05-24 12:47:54 -0400

    update create workshop form

--- app/Workshop.php
+++ app/Workshop.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Workshop extends Model
 {
-    protected $fillable = ["name", "description", "date", "start_time", "end_time", "room_id"];
+    protected $fillable = ["name", "description", "date", "start_time", "end_time", "room_id", "cover_image"];
 
     public function room() {
         return $this->belongsTo(Rooms::class);
--- config/dash.php
+++ config/dash.php
@@ -1,32 +1,36 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: Shawn
-     * Date: 3/17/2017
-     * Time: 6:13 PM
+/**
+ * Created by PhpStorm.
+ * User: Shawn
+ * Date: 3/17/2017
+ * Time: 6:13 PM
+ */
+
+return [
+
+    /*
+     *
+     * Configure how forms/fields for each table are generated
      */
 
-    return [
+    'forms' => [
 
-        /*
-         *
-         * Configure how forms/fields for each table are generated
-         */
-
-        'forms' => [
-
-            "users" => [
-                "field_types" => ["password" => "password", "id" => "hidden", "email" => "email", 'region_id' => 'select'],
-                "labels" => [
-                    "password" => "Your Login Password",
-                    "email" => "Your Email",
-                    "region_id" => "Which CareerSource Region  Are You From"
-                ],
-                "options" => [
-                    'region_id' => ['', 'Suncoast', 'pascoHernando', 'Tampa', 'Miami']
-                ]
+        "users" => [
+            "field_types" => ["password" => "password", "id" => "hidden", "email" => "email", 'region_id' => 'select'],
+            "labels" => [
+                "password" => "Your Login Password",
+                "email" => "Your Email",
+                "region_id" => "Which CareerSource Region  Are You From"
+            ],
+            "options" => [
+                'region_id' => ['', 'Suncoast', 'pascoHernando', 'Tampa', 'Miami']
             ]
-
+        ],
+        "workshops" => [
+            "field_types" => ["date" => "date", 'cover_image' => 'file'],
+            "labels" => ["cover_image" => "Cover Image (jpg, png, Max (20mb)"]
         ]
 
-    ];
+    ]
+
+];
