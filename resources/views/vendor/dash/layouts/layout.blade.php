a06b5fc85fb7775141f78c8d505b899e2499313e
shawn sandy <shawnsandy04@gmail.com>
2017-05-24 14:42:38 -0400

    add cleave.js move js scripts

--- resources/views/vendor/dash/layouts/layout.blade.php
+++ resources/views/vendor/dash/layouts/layout.blade.php
@@ -13,7 +13,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
     <![endif]-->
     <!-- Latest compiled and minified CSS -->
-
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/messenger/1.5.0/css/messenger.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/messenger/1.5.0/css/messenger-theme-block.css">
@@ -119,12 +118,20 @@
 <!-- Latest compiled and minified JavaScript -->
 
 <script src="//code.jquery.com/jquery-1.12.4.js"></script>
-
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+<script src="//unpkg.com/cleave.js@0.7.16/dist/cleave.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
         crossorigin="anonymous">
 </script>
 
+@stack('inline-scripts')
+<script src="/js/admin/chart.min.js"></script>
+<script src="/js/admin/chart-data.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/messenger/1.5.0/js/messenger.js"></script>
+<script src="/js/flash.js"></script>
 @stack('scripts')
 
 <!-- Menu Toggle Script -->
@@ -136,13 +143,6 @@
     });
 </script>
 
-@stack('inline-scripts')
-<script src="/js/admin/chart.min.js"></script>
-<script src="/js/admin/chart-data.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.js"></script>
-<script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/messenger/1.5.0/js/messenger.js"></script>
-<script src="/js/flash.js"></script>
 <script>
     $('#calendar').datepicker({
     });
@@ -161,7 +161,7 @@
         if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
     })
 </script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+
 {!! Toastr::render() !!}
 
 @include('assets.pusher.pusher')
