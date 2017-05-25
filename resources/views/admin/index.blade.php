2f96593a60d9e242fa1965e3aa1ccff6390ee620
Unknown <steve@ninjagamehost.com>
2017-05-24 21:10:45 -0700

    Chart now shows live data from new API. Removed old Pie charts.

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -73,53 +73,67 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Site Traffic Overview</div>
+                    <div class="panel-heading">User Registrations</div>
                     <div class="panel-body">
                         <div class="canvas-wrapper">
-                            <canvas class="main-chart" id="line-chart" height="200" width="600"></canvas>
+                            <div id="users" style="height: 500px;"></div>
                         </div>
                     </div>
                 </div>
             </div>
         </div><!--/.row-->
 
-        <div class="row">
-            <div class="col-xs-6 col-md-3">
-                <div class="panel panel-default">
-                    <div class="panel-body easypiechart-panel">
-                        <h4>New Orders</h4>
-                        <div class="easypiechart" id="easypiechart-blue" data-percent="92" ><span class="percent">92%</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-6 col-md-3">
-                <div class="panel panel-default">
-                    <div class="panel-body easypiechart-panel">
-                        <h4>Comments</h4>
-                        <div class="easypiechart" id="easypiechart-orange" data-percent="65" ><span class="percent">65%</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-6 col-md-3">
-                <div class="panel panel-default">
-                    <div class="panel-body easypiechart-panel">
-                        <h4>New Users</h4>
-                        <div class="easypiechart" id="easypiechart-teal" data-percent="56" ><span class="percent">56%</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-6 col-md-3">
-                <div class="panel panel-default">
-                    <div class="panel-body easypiechart-panel">
-                        <h4>Visitors</h4>
-                        <div class="easypiechart" id="easypiechart-red" data-percent="27" ><span class="percent">27%</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div><!--/.row-->
     </div>	<!--/.main-->
+    @include('assets.dashboard.charts')
+
+    @push('scripts')
+    <script>
+        var start = moment().subtract(30, 'days').format('YYYY-MM-DD');
+        var end = moment().add(1, 'days').format('YYYY-MM-DD');
+        $(function() {
+            // Create a function that will handle AJAX requests
+            function requestData(days, chart){
+                $.ajax({
+                    type: "GET",
+                    dataType: 'json',
+                    url: "/api/users/"+start+"/"+end // This is the URL to the API
+                })
+                    .done(function( data ) {
+                        // When the response to the AJAX request comes back render the chart with new data
+                        chart.setData(data);
+                    })
+                    .fail(function() {
+                        // If there is no communication between the server, show an error
+                        alert( "error occurred" );
+                    });
+            }
+            var chart = new Morris.Area({
+                // ID of the element in which to draw the chart.
+                element: 'users',
+                parseTime:false,
+                behaveLikeLine:true,
+                data: [0, 0], // Set initial data (ideally you would provide an array of default data)
+                xkey: 'date', // Set the key for X-axis
+                ykeys: ['users'], // Set the key for Y-axis
+                lineColors: ['#55AEF8'],
+                labels: ['Registrations'] // Set the label when bar is rolled over
+            });
+
+            // Request initial data for the past 7 days:
+            requestData(90, chart);
+
+            $('ul.open-ranges a').click(function(e){
+                e.preventDefault();
+
+                // Get the number of days from the data attribute
+                var el = $(this),
+                    days = el.attr('data-range');
+
+                // Request the data and render the chart using our handy function
+                requestData(days, chart);
+            })
+        });
+
+    </script>
+    @endpush
 @endsection
