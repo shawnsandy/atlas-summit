71552d708b13859367d288fad466020b497833d3
Unknown <steve@ninjagamehost.com>
2017-05-24 18:59:04 -0700

    Created Modal for delete and  added new fields  on Rooms index page.

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -21,21 +21,55 @@
                         <table class="table datatables">
                             <thead>
                             <tr>
-                                <td>ID</td>
                                 <td>Name</td>
+                                <td>Capacity</td>
                                 <td>Created</td>
+                                <td>Edit\Delete</td>
                             </tr>
                             </thead>
                             <tbody>
                             @foreach($rooms as $room)
                                 <tr>
-                                    <td>{{ $room->id }}</td>
                                     <td>{{ $room->name }}</td>
-                                    <td>{{ $room->created_at }}</td>
+                                    <td>{{ $room->capacity }}</td>
+                                    <td>{{ date('F d, Y g:i A', strtotime($room->created_at)) }}</td>
+                                    <td>
+                                        <div class="btn-group pull-right">
+                                            <a href="/admin/rooms/{{ $room->id }}/edit" type="button" class="btn btn-default">Edit</a>
+                                            <a data-target="#modal-{{ $room->id }}" data-toggle="modal" type="button" class="btn btn-default">Delete</a>
+
+                                            <div class="modal fade" id="modal-{{ $room->id }}"
+                                                 tabIndex="-1">
+                                                <div class="modal-dialog" style="width: 50%; height: 80%">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <button type="button" class="close"
+                                                                    data-dismiss="modal">
+                                                                ×
+                                                            </button>
+                                                            <h4 class="modal-title">Are you sure?</h4>
+                                                        </div>
+                                                        <div class="modal-body text-center">
+                                                            <h3>Are you sure you want to remove {{ $room->name }}?</h3>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <form method="post" action="/admin/regions/{{ $room->id }}"> {{method_field('DELETE')}} {{csrf_field()}}
+                                                                <button type="button" class="btn btn-default"
+                                                                        data-dismiss="modal">Go Back
+                                                                </button>
+                                                                <button class="btn btn-danger" type="submit">
+                                                                    Delete
+                                                                </button>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
                                 </tr>
                             @endforeach
                             </tbody>
-                        </table>
                         </table>
                     </div>
                 </div>
