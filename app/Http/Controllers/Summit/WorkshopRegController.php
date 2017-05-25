7a8380d54fe89430d2968d1f1cd27d50642d5884
shawnsandy04 <shawnsandy04@gmail.com>
2017-05-25 01:33:49 -0400

    workshop registration controller

---
+++ app/Http/Controllers/Summit/WorkshopRegController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Summit;
+
+use App\User;
+use App\Workshop;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class WorkshopRegController extends Controller
+{
+    public function __invoke($workshop_id)
+    {
+        $ws = Workshop::find($workshop_id);
+        $user = User::first();
+        $ws->users()->attach($user->id);
+
+       return  back()->with("success", "You are registered for this workshop!");
+
+    }
+}
