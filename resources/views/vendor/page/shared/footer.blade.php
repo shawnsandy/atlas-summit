cedc5f5e3c9dd929ba52577a468a86dc0c100e61
shawn sandy <shawnsandy04@gmail.com>
2017-05-26 09:54:27 -0400

    add link to ctsfla website

--- resources/views/vendor/page/shared/footer.blade.php
+++ resources/views/vendor/page/shared/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="" data-aos="fade">
     <div class="layout-table">
         <div class="layout-row">
-<p>
-    <img src="https://ctsfla.com/img/cts-logo-nav.png" alt="">
-</p>
             <p>
+                <a href="//ctsfla.com"><img src="https://ctsfla.com/img/cts-logo-nav.png" alt=""></a>
 
-                <small> Designed and developed by Complete Technology Solutions</small>
+            </p>
+            <p>
+                <small> Designed and developed by <a href="//ctsfla.com">Complete Technology Solutions</a></small>
             </p>
         </div>
     </div>
