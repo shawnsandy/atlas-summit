4551c9ed4bda4557f24a5b817e642e84d1b96f9b
shawnsandy04 <shawnsandy04@gmail.com>
2017-05-27 08:32:20 -0400

    Feature - bio validation request

---
+++ app/Http/Requests/BioRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BioRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "biography" => "required|max:500",
+            "job_title" => "required|max:200",
+            "avatar" => "image:size:5000",
+        ];
+    }
+}
