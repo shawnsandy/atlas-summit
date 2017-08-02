9ae4d9e564bf2b8c10c001c74ebd4153b92c7d0e
shawn sandy <shawnsandy04@gmail.com>
2017-08-02 10:25:58 -0400

    merge updates

--- resources/views/theme/page/shared/cover-fold.blade.php
+++ resources/views/theme/page/shared/cover-fold.blade.php
@@ -1,21 +1,15 @@
-<div class="cover-fold  bg-5">
+<div class="cover-fold">
     @include('page::shared.nav')
     <header class="{{ $class_name or " main " }}" data-aos="fade-up">
-        <section class="cover">
-            <div class="layout-table">
-                <div class="layout-row">
-                    <div class="logo">
-
-                        @if(config('pakgekit.branding') && config('pakgekit.brand.logo'))
-                        <img src="/img/{{ config('pagekit.brand.logo' )}}" title="{{ config('pagekit.company_name') }}" />                        @endif
-
-                        <h1 class="oswald" style="font-family: Oswald">ENGAGE.EDUCATE.EMPOWER.</h1>
-
-                        <p class="lead text-uppercase oswald"> Building Tomorrows Workforce </p>
-
-                    </div>
+        <div class="flex-center">
+            <div class="text-center" style="color: #fff;">
+                <div class="col-md-12">
+                    <h1 class="oswald" style="font-family: Oswald">ENGAGE . EDUCATE . EMPOWER .</h1>
+                </div>
+                <div class="col-md-12 ">
+                    <p class="lead text-uppercase oswald"> Building Tomorrows Workforce </p>
                 </div>
             </div>
-        </section>
+        </div>
     </header>
 </div>
