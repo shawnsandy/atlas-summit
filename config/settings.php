827af85faef62993ae7c49380fdd0f9aabe00cb5
shawn sandy <shawnsandy04@gmail.com>
2017-08-09 08:16:51 -0400

    Update - add sponsors.levels array to settings

--- config/settings.php
+++ config/settings.php
@@ -5,3 +5,15 @@
  * Date: 8/9/17
  * Time: 8:07 AM
  */
+
+
+return [
+  'sponsors' => [
+      'levels' => [
+          'Premiere' => "Premier",
+          'Titanium' => "Titanium",
+          'Gold' => "Gold",
+          'Sliver' => "Sliver",
+      ]
+  ]
+];
