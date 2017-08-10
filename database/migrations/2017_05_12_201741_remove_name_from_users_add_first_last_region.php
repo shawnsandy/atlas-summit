f0f71033da3bad0dd66b40669b953b0a2547ce8d
shawn sandy <shawnsandy04@gmail.com>
2017-08-10 10:47:15 -0400

    update - add timestamps to scans  migrations

--- database/migrations/2017_05_12_201741_remove_name_from_users_add_first_last_region.php
+++ database/migrations/2017_05_12_201741_remove_name_from_users_add_first_last_region.php
@@ -18,6 +18,7 @@ class RemoveNameFromUsersAddFirstLastRegion extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->integer('region_id');
+            $table->timestamps();
         });
     }
 
