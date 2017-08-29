6dca238282dd5eb29e4053f0ec6718bf907280ce
shawn sandy <shawnsandy04@gmail.com>
2017-08-29 10:27:44 -0400

    update : add name attribute to form::open(), label property to button

--- resources/views/imports/form.blade.php
+++ resources/views/imports/form.blade.php
@@ -1,9 +1,9 @@
-{{ Form::open(['url' => "$url", 'files' => true]) }}
+{{ Form::open(['url' => "$url", 'files' => true, "name" => "$url"]) }}
 <p class="">
     {{ Form::file("cvs",  ["class" => "form-control file-input"]) }}
 </p>
 {!! $errors->first('cvs', '<p class="text-danger">:message</p>') !!}
-<p class="">
-    <button class="btn btn-default">Import</button>
+<p class="text-right">
+    <button class="btn btn-primary btn-xs">{{ $label or "Import CSV" }}</button>
 </p>
 {{ Form::close() }}
