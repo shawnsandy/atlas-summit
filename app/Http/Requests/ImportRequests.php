d0c04487c693fe7ebef47a800c8da41edc3b2e79
shawnsandy04 <shawnsandy04@gmail.com>
2017-08-28 21:09:34 -0400

    fix : typo cvs

--- app/Http/Requests/ImportRequests.php
+++ app/Http/Requests/ImportRequests.php
@@ -28,7 +28,7 @@ class ImportRequests extends FormRequest
     public function rules()
     {
         return [
-            'cvs' => 'required|file|mimes:csv'
+            'cvs' => 'required|file|mimes:cvs'
         ];
     }
 
