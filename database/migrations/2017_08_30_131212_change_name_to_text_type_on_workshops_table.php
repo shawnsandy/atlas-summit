4a1ad5df011654acd95a9258162673814a058dc6
shawn sandy <shawnsandy04@gmail.com>
2017-08-30 15:28:50 -0400

    migration : change file name

--- database/migrations/2017_08_30_131212_change_name_to_text_type_on_workshops_table.php
+++ database/migrations/2017_08_30_131212_change_name_to_text_type_on_workshops_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeNameToTextTypeOnWorkshopsTable01 extends Migration
+class ChangeNameToTextTypeOnWorkshopsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class ChangeNameToTextTypeOnWorkshopsTable01 extends Migration
     public function up()
     {
         Schema::table('workshops', function (Blueprint $table) {
-            $table->dropColumn('name');
+            //$table->dropColumn('name');
             $table->string("name");
         });
     }
