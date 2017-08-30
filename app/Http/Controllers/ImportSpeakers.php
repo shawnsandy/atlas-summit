fd9c6d5a8b5a347564ee8ac4ebeb58f6baf35fc2
shawn sandy <shawnsandy04@gmail.com>
2017-08-30 09:37:18 -0400

    Feature - speaker upload controller

---
+++ app/Http/Controllers/ImportSpeakers.php
@@ -0,0 +1,35 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: shawnsandy
+     * Date: 8/28/17
+     * Time: 2:23 PM
+     */
+
+    namespace App\Http\Controllers;
+
+
+    use App\Http\Requests\ImportRequests;
+    use Illuminate\Http\Request;
+    use Laracasts\Flash\Flash;
+
+    class ImportSpeakers extends Controller
+    {
+
+        public function store(ImportRequests $request)
+        {
+
+            if ($request->hasFile("cvs")) {
+
+                $data = $request->imports(20, 0);
+
+                return dump($data);
+
+            }
+
+
+            return back()->with("error", "Failed to import file");
+
+        }
+
+    }
