2ac2c3d74dcf642ef66044542fc9eb068f91e175
shawn sandy <shawnsandy04@gmail.com>
2017-08-30 09:36:41 -0400

    update : add speakers-csv to routes

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,8 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'can:admin-ability']
 
     Route::resource("workshop-cvs", "ImportWorkshops", ["only" => ["store"]]);
 
+    Route::resource("speakers-cvs", "ImportSpeakers", ["only" => ["store"]]);
+
     Route::resource("users-cvs", "ImportUsersController", ["only" => ["store"]]);
 
 
